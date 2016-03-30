23e89ea678d2378cc04a222ad273d904e8857654
Soichi Hayashi <hayashis@iu.edu>
2016-03-30 20:35:10 +0000

    added check

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 module load nodejs
-node genpbs.js > submit.pbs
+node $SCA_SERVICE_DIR/genpbs.js > submit.pbs
 jobid=`qsub submit.pbs`
 echo $jobid > jobid
 curl -X POST -H "Content-Type: application/json" -d "{\"status\": \"waiting\", \"msg\":\"Job: $jobid Waiting in PBS queue on $execenv\"}" $SCA_PROGRESS_URL
