b14e189f2538fa8712a37ecd13f214df87de8497
Soichi Hayashi <hayashis@iu.edu>
2016-03-30 20:27:22 +0000

    implemented rc

---
+++ genpbs.js
@@ -0,0 +1,24 @@
+#!/bin/env node
+'use strict';
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('config.json', "utf8"));
+
+function genrecon(file) {
+    var subject = file.filename.substring(0, file.filename.length-4);
+    var line = "recon-all -i \"../"+config.input_task_id+"/"+file.filename+"\" -subject \""+subject+"\"";
+    if(config.a) line+=" -all";
+    if(config.hipposubfields) line+=" -hippo-subfields";
+    line += " &\n";
+    return line;
+}
+
+var template = fs.readFileSync(__dirname+"/template.pbs", "utf8");
+
+var reconall = "";
+config.files.forEach(function(file) {
+    reconall += genrecon(file);
+});
+template.replace("__reconall__", reconall);
+
+console.log(template);
---
+++ test/start.sh
@@ -0,0 +1,1 @@
+../genpbs.js
