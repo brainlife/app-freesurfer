460ac510330771f981edc0af90c1b2d3e713c681
Soichi Hayashi <hayashis@iu.edu>
2016-04-13 13:16:40 +0000

    changed the location of -all option to see if it kicks in

--- genpbs.js
+++ genpbs.js
@@ -11,8 +11,7 @@ function genrecon(file) {
 
     var subject = file.filename.substring(0, file.filename.length-4);
     var workdir = process.env.SCA_WORKFLOW_DIR;
-    var line = "recon-all -i \""+workdir+"/"+config.input_task_id+"/"+file.filename+"\" -subject \""+subject+"\"";
-    line+=" -all"; //recon-all without -all doesn't make sense..
+    var line = "recon-all -all -i \""+workdir+"/"+config.input_task_id+"/"+file.filename+"\" -subject \""+subject+"\"";
     if(config.hipposubfields) line+=" -hippo-subfields";
     line += " &\n";
     return line;
