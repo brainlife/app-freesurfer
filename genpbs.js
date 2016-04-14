e481cc09aa748d587f495846851234004d497ada
Soichi Hayashi <hayashis@iu.edu>
2016-04-14 21:32:56 +0000

    updated to output fake products.json using real subject name from config.json (even if the job fails for that subject..)

--- genpbs.js
+++ genpbs.js
@@ -5,35 +5,37 @@ var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('config.json', "utf8"));
 
 var template = fs.readFileSync(__dirname+"/template.pbs", "utf8");
+var products = [];
 
-function genrecon(file) {
+function genrecon(file, subject) {
     var proc = config.process; //TODO - deal with this.. (recon, etc..)
 
-    var subject = file.filename.substring(0, file.filename.length-4);
     var workdir = process.env.SCA_WORKFLOW_DIR;
     var line = "recon-all -all -i \""+workdir+"/"+config.input_task_id+"/"+file.filename+"\" -subject \""+subject+"\"";
     if(config.hipposubfields) line+=" -hippo-subfields";
     line += " &\n";
     return line;
 }
-function genzip(file) {
-    var subject = file.filename.substring(0, file.filename.length-4);
+
+function genzip(file, subject) {
     var workdir = process.env.SCA_WORKFLOW_DIR;
     var line = "zip -rm \""+subject+".zip\" \""+subject+"\"\n";
     return line;
 }
 
-
 var reconall = "";
 var zip = "";
 config.files.forEach(function(file) {
-    reconall += genrecon(file);
-    zip += genzip(file);
+    var subject = file.filename.substring(0, file.filename.length-4);
+    reconall += genrecon(file, subject);
+    zip += genzip(file, subject);
+    products.push({type: "freesurfer", dir: subject});
 });
 
 //do substitutions
 template = template.replace(/__taskdir__/g, process.env.SCA_TASK_DIR);
 template = template.replace("__reconall__", reconall);
 template = template.replace("__zip__", zip);
+template = template.replace("__products__", JSON.stringify(products));
 
 console.log(template);
