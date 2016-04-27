ae415fe12dc8f2cf71fef056ac0d7f0b6811a1d2
Soichi Hayashi <hayashis@iu.edu>
2016-04-27 19:30:43 +0000

    started to remove unnecesary for loop

--- test/json.sh
+++ test/json.sh
@@ -1,5 +1,8 @@
 cd ..
-Rscript statsex/RunAll.r 
+#RunAll.r <basedir> <temp_dir>
+rm -rf /usr/local/tmp/DICOM_QC_FreeSurfer/10142_3_RES/Results
+mkdir /usr/local/tmp/DICOM_QC_FreeSurfer/10142_3_RES/Results
+Rscript statsex/RunAll.r /usr/local/tmp/DICOM_QC_FreeSurfer/10142_3_RES /home/hayashis/git/sca-service-freesurfer/statsex_template
 #/usr/local/tmp/DICOM_QC_FreeSurfer/10142_3_RES > /usr/local/tmp/DICOM_QC_FreeSurfer/10142_3_RES/.analyzed.json
 
 
