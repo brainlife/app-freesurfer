b1b9847887222aee39e8f526d8c2c4e67d972009
Soichi Hayashi <hayashis@iu.edu>
2016-12-09 16:07:34 -0500

    added back jq

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #make sure jq is installed on $SCA_SERVICE_DIR (used by status.sh to analyze progress)
-#if [ ! -f $SCA_SERVICE_DIR/jq ];
-#then
-#        echo "installing jq"
-#        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
-#        chmod +x $SCA_SERVICE_DIR/jq
-#fi
+if [ ! -f $SCA_SERVICE_DIR/jq ];
+then
+        echo "installing jq"
+        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
+        chmod +x $SCA_SERVICE_DIR/jq
+fi
 
 #mainly to debug locally
 if [ -z $SCA_WORKFLOW_DIR ]; then export SCA_WORKFLOW_DIR=`pwd`; fi
