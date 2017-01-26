008bb92e0b64f534754192096bcaa3790581fd67
Soichi Hayashi <hayashis@iu.edu>
2017-01-26 16:03:32 -0500

    added jq install back

--- start.sh
+++ start.sh
@@ -6,14 +6,13 @@ if [ -z $SCA_TASK_DIR ]; then export SCA_TASK_DIR=`pwd`; fi
 if [ -z $SCA_SERVICE_DIR ]; then export SCA_SERVICE_DIR=`pwd`; fi
 if [ -z $SCA_PROGRESS_URL ]; then export SCA_PROGRESS_URL="https://soichi7.ppa.iu.edu/api/progress/status/_sca.test"; fi
 
-#jq is part of the repo now
 #make sure jq is installed on $SCA_SERVICE_DIR (used by status.sh to analyze progress)
-#if [ ! -f $SCA_SERVICE_DIR/jq ];
-#then
-#        echo "installing jq"
-#        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
-#        chmod +x $SCA_SERVICE_DIR/jq
-#fi
+if [ ! -f $SCA_SERVICE_DIR/jq ];
+then
+        echo "installing jq"
+        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
+        chmod +x $SCA_SERVICE_DIR/jq
+fi
 
 #mainly to debug locally
 if [ -z $SCA_WORKFLOW_DIR ]; then export SCA_WORKFLOW_DIR=`pwd`; fi
