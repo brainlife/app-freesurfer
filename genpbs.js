4ecad726e3a344c2288a52721d4a1383fe2ad9e3
Soichi Hayashi <hayashis@iu.edu>
2017-06-16 08:25:53 -0400

    updated to run on carbonate

--- genpbs.js
+++ genpbs.js
@@ -7,9 +7,11 @@ const path = require('path');
 
 //setup environment specific pbs parameters
 var pbs = "";
-if(process.env.PBS_ENV == "karst") {
+if(process.env.HPC == "KARST") {
     pbs += "#PBS -l nodes=1:ppn=16,walltime=12:00:00\n";
-} else if(process.env.PBS_ENV == "bigred2") {
+} else if(process.env.HPC == "CARBONATE") {
+    pbs += "#PBS -l nodes=1:ppn=16,walltime=6:00:00\n";
+} else if(process.env.HPC == "BIGRED2") {
     pbs += "#PBS -l nodes=1:ppn=32\n#PBS -q cpu\n#PBS -l walltime=20:00:00\n";//16 hours not enough
 }
 
@@ -19,22 +21,20 @@ function genrecon(filename, subject) {
     //recon-all fails if there is previous output dir (according to Jonn?)
     line += "rm -rf "+subject+"\n";
 
-    //if(process.env.PBS_ENV == "bigred2") line += "module load ccm\nccmrun ";
-    if(process.env.PBS_ENV == "bigred2") line += "export OMP_NUM_THREADS=32\naprun -n 1 -d 32 ";
-    //line += "recon-all -i \""+filename+"\" -subject \""+subject+"\" -autorecon1 -autorecon2 -openmp 16";
+    if(process.env.HPC == "BIGRED2") line += "export OMP_NUM_THREADS=32\naprun -n 1 -d 32 ";
+
     line += "recon-all -i \""+filename+"\" -subject \""+subject+"\" -all"; //all is needed to generate all labels
-    if(process.env.PBS_ENV == "bigred2") line += " -openmp 32";
-    if(process.env.PBS_ENV == "karst") line += " -openmp 16"; //not sure if this really does anything on karst
+    if(process.env.HPC == "BIGRED2") line += " -openmp 32";
+    if(process.env.HPC = "KARST") line += " -openmp 16"; //not sure if this really does anything on karst
+    if(process.env.HPC == "CARBONATE") line += " -openmp 16"; 
     if(config.hipposubfields) line+=" -hippo-subfields";
     return line;
 }
 
 var reconall = genrecon(config.t1, "output");
-//products.push({type: "freesurfer", dir: "output"});
 
 //do substitutions
 var template = fs.readFileSync(__dirname+"/template.pbs", "utf8");
-template = template.replace(/__taskdir__/g, process.env.TASK_DIR);
 template = template.replace("__pbs__", pbs);
 template = template.replace("__reconall__", reconall);
 
--- start.sh
+++ start.sh
@@ -2,26 +2,24 @@
 
 #allows test execution
 if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
-if [ -z $TASK_DIR ]; then export TASK_DIR=`pwd`; fi
+if [ -z $ENV ]; then export ENV=IUHPC; fi
 
-#find out which environment we are in
-hostname | grep karst > /dev/null
-if [ $? -eq 0 ]; then
-    export PBS_ENV=karst
+if [ $ENV == "IUHPC" ]; then
+	module load nodejs
 fi
-echo $HOME | grep -i bigred > /dev/null
-if [ $? -eq 0 ]; then
-    export PBS_ENV=bigred2
-fi
-
-echo "Generating submit.pbs"
-module load nodejs
 node $SERVICE_DIR/genpbs.js > submit.pbs
 
 #clean up previous job (just in case)
 rm -f finished
-echo "Submitting.."
-jobid=`qsub submit.pbs`
-echo $jobid > jobid
 
-#curl -s -X POST -H "Content-Type: application/json" -d "{\"status\": \"waiting\", \"msg\":\"Job: $jobid Waiting in PBS queue on $execenv\"}" $SCA_PROGRESS_URL > /dev/null
+if [ $ENV == "IUHPC" ]; then
+	echo "Submitting.."
+	jobid=`qsub submit.pbs`
+	echo $jobid > jobid
+fi
+
+if [ $ENV == "VM" ]; then
+    nohup time $SERVICE_DIR/submit.pbs > stdout.log 2> stderr.log &
+    echo $! > pid
+fi
+
