77dfcb93738305cd54dd769d9d8b4566e126e108
Soichi Hayashi <hayashis@iu.edu>
2019-10-24 14:33:53 +0000

    added label.json

--- genkey.js
+++ genkey.js
@@ -1,12 +1,26 @@
+#!/usr/bin/env node
 const fs = require('fs');
 
-let lut = fs.readFileSync("/N/soft/rhel7/freesurfer/6.0.0/freesurfer/FreeSurferColorLUT.txt", "ascii");
+let lut = fs.readFileSync("FreeSurferColorLUT.txt", "ascii");
+
+let key = "";
+let labels = [];
 
 lut.split("\n").forEach(line=>{
 	line = line.trim();
 	if(line[0] == "#") return;
 	if(line == "") return;
 	let tokens = line.split(/[ ]+/);
-	console.log(tokens[0]+" -> "+tokens[0] + " == "+tokens[1]);
+	key += tokens[0]+" -> "+tokens[0] + " == "+tokens[1]+"\n";
+    labels.push({
+        "name": tokens[1],
+        "label": tokens[0],
+        "voxel_value": parseInt(tokens[0]),
+        "r": parseInt(tokens[2]),
+        "g": parseInt(tokens[3]),
+        "b": parseInt(tokens[4]),
+    });
 });
 
+fs.writeFileSync("label.json", JSON.stringify(labels, null, 4));
+fs.writeFileSync("key.txt", key);
